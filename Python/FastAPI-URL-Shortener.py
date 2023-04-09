25e28e0bc439586f83fed055038b869b9f6808a5
Kevin Tornquist <kevin.tornquist@gmx.de>
2023-04-09 11:01:50 +0200

    1. encode Endpoint um URLs zu kürzen

--- Python/FastAPI-URL-Shortener.py
+++ Python/FastAPI-URL-Shortener.py
@@ -14,3 +14,21 @@ url = []
 def gen_key(amount: int) -> str:
     return ''.join(random.choice(string.ascii_letters+string.digits) for _ in range(amount))
 
+# api
+app = FastAPI()
+short = "http://short.est/"
+
+@app.post("/encode")
+def encode(input_url: ShortenURL):
+    print(input_url.url)
+    # Überprüfe ob URL schon eine gekürzte URL hat
+    for x in url:
+        if x["url"] == input_url.url:
+            return["short_url"]
+    # Speichere gekürzte URL in Pseudodatenbank
+    output = input_url.dict()
+    short_url = short + gen_key(6)
+    output.update({"short_url": short_url})
+    url.append(output)
+    return {"short_url": short_url}
+
