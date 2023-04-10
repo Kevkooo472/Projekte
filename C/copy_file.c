21fae5c86ad382355a7561eacb1cb426fc8e5d0b
Kevin Tornquist <kevin.tornquist@gmx.de>
2023-04-10 20:28:07 +0200

    Funktion copy_text implementiert Überprüfung auf Pufferfehler

--- C/copy_file.c
+++ C/copy_file.c
@@ -17,7 +17,7 @@ int copy_file(FILE *quelle, char *filename)
     /* Ueberpruefen ob Dateizugriff moeglich ist */
     quelle = fopen(filename, "r");
     if (quelle == NULL) {
-        printf("Oeffnen der Textdatei '%s' nicht moeglich.\n", filename);
+        printf("Oeffnen der Textdatei '%s' fehlgeschlagen.\n", filename);
         return 0;
     }
     ziel = fopen("kopie.txt", "w");
@@ -35,6 +35,17 @@ int copy_file(FILE *quelle, char *filename)
         ++i;
         read = fgetc(quelle);
     }
-    
-
+    if (fclose(quelle) != 0) {
+        printf("Schliessen der Quelldatei '%s' fehlgeschlagen.\n", filename);
+        return 0;
+    }
+    if (fclose(ziel) != 0) {
+        printf("Schliessen der Kopie fehlgeschlagen.\n");
+        return 0;
+    }
+    if (read == EOF && feof(quelle) == 0) {
+        printf("EOF\n");
+        return -1;
+    }
+    return 1;
 }
