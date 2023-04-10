98a7c6be3352de62aef237e2efea52fe2d283e67
Kevin Tornquist <kevin.tornquist@gmx.de>
2023-04-10 20:22:33 +0200

    Schreiben in Zieldatei

--- C/copy_file.c
+++ C/copy_file.c
@@ -12,7 +12,7 @@ int main(void)
 
 int copy_file(FILE *quelle, char *filename)
 {
-    FILE *copy;
+    FILE *ziel;
     int read, write, i = 0;
     /* Ueberpruefen ob Dateizugriff moeglich ist */
     quelle = fopen(filename, "r");
@@ -20,12 +20,21 @@ int copy_file(FILE *quelle, char *filename)
         printf("Oeffnen der Textdatei '%s' nicht moeglich.\n", filename);
         return 0;
     }
-    copy = fopen("kopie.txt", "w");
-    if (copy == NULL) {
+    ziel = fopen("kopie.txt", "w");
+    if (ziel == NULL) {
         printf("Oeffnen der Kopie fehlgeschlagen.\n");
         return 0;
     }
     /* Quell-Textdatei einlesen */
     read = fgetc(quelle);
+    while (read != EOF) {
+        /* Schreiben in Zieldatei */
+        write = fputc(read, ziel);
+        if (write != read || write == EOF)
+            break;
+        ++i;
+        read = fgetc(quelle);
+    }
+    
 
 }
