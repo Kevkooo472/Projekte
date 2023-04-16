ec7f35202c308aef65e189dcb5870ac61c83a2c1
Kevin Tornquist <kevin.tornquist@gmx.de>
2023-04-16 18:01:04 +0200

    Makro und Konstanten

---
+++ C/Fließkommacodierung/fk.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define DIM 16
+#define VALID_INPUT 0
+#define INVALID_INPUT -3
+#define BUFFER_ERROR -2
+
+#define VAL_LENGTH(i, DIM) (i < DIM)
